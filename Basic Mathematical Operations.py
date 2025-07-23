51402d04cb8e341cc8c298a63bfc281ab97e9fa2
Dhruv Kakadiya <70765945+dhruv3501@users.noreply.github.com>
2025-07-23 15:57:09 +0530

    Add files via upload

---
+++ Basic Mathematical Operations.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your first number : "))
+b = int(input("Enter your second number : "))
+
+print("addition : " + str(a+b))
+print("subtraction : " + str(a-b))
+print("multiplication: " + str(a*b))
+print("division: " + str(a/b))
---
+++ Personalize Greeting.py
@@ -0,0 +1,4 @@
+name1 = input("Enter Your First Name : ")
+name2 = input("Enter Your Second Name : ")
+
+print("Hello" +" " + name1 + " " + name2 + " " + "Welcome to the python program.")
