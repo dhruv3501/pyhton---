a5907aa58e2fe0b1ee70b411c578cae42343f600
Dhruv Kakadiya <70765945+dhruv3501@users.noreply.github.com>
2025-07-24 20:05:53 +0530

    Add files via upload

---
+++ math module Calculator.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number : "))
+
+from math import sqrt
+print('square root : ', sqrt(num))
+
+from math import log
+print('Logeritham : ' , log(num))
+
+from math import sin
+print('Sine :', sin(num))
